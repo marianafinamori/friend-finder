853b538c524af810718ae5df968176b6978345d4
MadiGabrielle <marianafinamori@dyn-160-39-194-182.dyn.columbia.edu>
2019-04-13 14:34:30 -0400

    First commit to heroku

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 var PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/home.html"));
+});
 
 app.listen(PORT, ()=> {console.log("listening")});
